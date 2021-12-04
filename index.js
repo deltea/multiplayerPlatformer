a33b1115467e0cd8bc6a95437a30743cde2cf142
thcheetah777 <thcheetah777@gmail.com>
2021-12-03 17:59:12 -0800

    Init all

--- index.js
+++ index.js
@@ -5,16 +5,45 @@ const server = http.createServer(app);
 const {Server} = require("socket.io");
 const io = new Server(server);
 
+// Last player ID
+server.lastPlayerID = 0;
+
+// Get all player instances
+const getAllPlayers = () => {
+  let players = [];
+  console.log(`connected players: ${Object.keys(io.sockets.sockets)}`);
+  Object.keys(io.sockets.sockets).forEach((socketID) => {
+    let player = io.sockets.sockets[socketID].player;
+    if (player) {
+      players.push(player);
+    }
+  });
+  return players;
+}
 app.get("/", (request, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 app.use(express.static(__dirname));
+let players = {};
 io.on("connection", (socket) => {
   console.log("A user connected");
   socket.on("disconnect", () => {
     console.log("A user disconnected");
   });
+  socket.on("playerMovement", function(data) {
+    players[socket.id].x = data.x;
+    players[socket.id].y = data.y;
+    players[socket.id].rotation = data.rotation;
+    socket.broadcast.emit("playerMoved", socket.players[socket.id]);
+  });
+  socket.on("newPlayer", () => {
+    socket.player = {
+      id: server.lastPlayerID++,
+      x: 500,
+      y: 800
+    };
+    socket.emit("currentPlayers", socket.player);
+    socket.broadcast.emit("newPlayer", socket.player);
+  });
 });
-server.listen(8001, () => {
-  console.log("listening on *:8001");
-});
+server.listen(8001);
--- script.js
+++ script.js
@@ -1,17 +1,153 @@
 // Multiplayer Platformer using Socket.io
+// All the variables
+let game = {
+  players: {}
+};
+
 // Preload: Load all the assets
 function preload() {
+  // Load image of player
+  this.load.image("playerJump", "assets/images/playerJump.png");
 
+  // Load walking animation
+  // Ninja Frog
+  this.load.spritesheet("ninjaFrogWalk", "assets/images/walk.png", {
+    // Proportions
+    frameWidth: 32,
+    frameHeight: 32
+  });
 }
 
 // Create: Create all the sprites and colliders
 function create() {
+  // Player group
+  game.otherPlayers = this.physics.add.group();
 
+  // Adds a new player to server
+  socket.on("currentPlayers", function(players) {
+    Object.keys(players).forEach(id => {
+      if (players[id].playerId === socket.id) {
+        game.addNewPlayer(self, players[id]);
+      } else {
+        game.addOtherPlayers(self, players[id]);
+      }
+    });
+  });
+  socket.on("newPlayer", function(playerInfo) {
+    game.addOtherPlayers(self, playerInfo);
+  });
+  socket.on("disconnect", function (playerId) {
+    game.otherPlayers.getChildren().forEach(function (otherPlayer) {
+      if (playerId === otherPlayer.playerId) {
+        otherPlayer.destroy();
+      }
+    });
+  });
+  socket.on("playerMoved", function(playerInfo) {
+    game.otherPlayers.getChildren().forEach(function(otherPlayer) {
+      if (playerInfo.playerId === otherPlayer.playerId) {
+        otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+      }
+    });
+  });
+  game.addNewPlayer = (self, playerInfo) => {
+    game.player = this.physics.add.sprite(playerInfo.x, playerInfo.y, "playerJump");
+    game.player.setCollideWorldBounds(true);
+  }
+  game.addOtherPlayers = (self, playerInfo) => {
+    const otherPlayer = this.physics.add.sprite(playerInfo.x, playerInfo.y, "playerJump");
+    otherPlayer.playerId = playerInfo.playerId;
+    otherPlayer.setCollideWorldBounds(true);
+    game.otherPlayers.add(otherPlayer);
+  }
+
+  // Input
+  game.cursors = this.input.keyboard.createCursorKeys();
+
+  // Create player sprite
+  // game.player = this.physics.add.sprite(100, 100, "playerJump");
+  Client.askNewPlayer();
+
+  // Bounds
+
+  // Animations
+  // Walk
+  this.anims.create({
+    // Animation key
+    key: "walk",
+
+    // Frames
+    frames: this.anims.generateFrameNumbers("ninjaFrogWalk", {
+      start: 0,
+      end: 11
+    }),
+
+    // Options
+    frameRate: 15,
+    repeat: -1
+  });
 }
 
 // Update: Update movement and animation
 function update() {
+  // Controls
+  if (game.player) {
+    // Right
+    if (game.cursors.right.isDown) {
+      // Move right
+      game.player.setVelocityX(200);
 
+      // Walk animation
+      game.player.anims.play("walk", true);
+
+      // Flip image
+      game.player.flipX = false;
+
+      // Dir var
+      game.player.dir = "R";
+
+      // Left
+    } else if (game.cursors.left.isDown) {
+      // Move left
+      game.player.setVelocityX(-200);
+
+      // Walk animation
+      game.player.anims.play("walk", true);
+
+      // Flip image
+      game.player.flipX = true;
+
+      // Dir var
+      game.player.dir = "L";
+
+      // None
+    } else {
+      // Don't move
+      game.player.setVelocityX(0);
+
+      // Stop anims
+      game.player.setTexture("playerJump");
+    }
+
+    // Key function
+    const keyPress = (key) => {
+      if (Phaser.Input.Keyboard.JustDown(this.input.keyboard.addKey(key))) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    // Jump
+    if (game.cursors.up.isDown && game.player.body.blocked.down) {
+      // Jump
+      game.player.setVelocityY(-800);
+    }
+    if (keyPress(Phaser.Input.Keyboard.KeyCodes.UP) && game.jumpsMade % 2 === 0 && !game.player.body.blocked.down && game.abilities.doubleJumps) {
+      // Jump
+      game.player.setVelocityY(-800);
+    }
+  }
 }
 
 // Phaser config
@@ -22,7 +158,7 @@ const config = {
   // Scaling
   scale: {
     autoCenter: Phaser.Scale.CENTER_BOTH,
-    mode: Phaser.Scale.FIT,
+    mode: Phaser.Scale.RESIZE,
   },
 
   // Rendering options
